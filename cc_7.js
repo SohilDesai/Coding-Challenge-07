3c9e45c70d3610f5eb576eb566536a515c4beebf
Sohil Desai <sohildesai@usf.edu>
2026-06-14 23:41:49 -0400

    Implement headline update on button click

--- cc_7.js
+++ cc_7.js
@@ -9,3 +9,18 @@ let ctaHeadline = document.getElementById("ctaHeadline");
 updateButton.addEventListener("click", function () {
 
 });
+let headlineInput = document.getElementById("headlineInput");
+let updateButton = document.getElementById("updateButton");
+let ctaHeadline = document.getElementById("ctaHeadline");
+
+updateButton.addEventListener("click", function () {
+
+    if (headlineInput.value !== "") {
+
+        ctaHeadline.textContent = headlineInput.value;
+
+        headlineInput.value = "";
+
+    }
+
+});
