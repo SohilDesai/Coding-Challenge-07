56c6b0569571202a0290ad62e6277ea3fc9dcb86
Sohil Desai <sohildesai@usf.edu>
2026-06-14 23:40:19 -0400

    Add event listener to update button

--- cc_7.js
+++ cc_7.js
@@ -2,3 +2,10 @@ console.log("Website Loaded");
 let headlineInput = document.getElementById("headlineInput");
 let updateButton = document.getElementById("updateButton");
 let ctaHeadline = document.getElementById("ctaHeadline");
+let headlineInput = document.getElementById("headlineInput");
+let updateButton = document.getElementById("updateButton");
+let ctaHeadline = document.getElementById("ctaHeadline");
+
+updateButton.addEventListener("click", function () {
+
+});
