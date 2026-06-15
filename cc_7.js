f825e64253dedf57980f0ae8a5d85474c89924cb
Sohil Desai <sohildesai@usf.edu>
2026-06-14 23:38:02 -0400

    Add elements for headline input and update button

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,4 @@
 console.log("Website Loaded");
+let headlineInput = document.getElementById("headlineInput");
+let updateButton = document.getElementById("updateButton");
+let ctaHeadline = document.getElementById("ctaHeadline");
